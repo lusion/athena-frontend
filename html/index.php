ac6989fdb54931360c476a2deec0072f46cf1daf
Josh Yudaken <j@yud.co.za>
2012-01-11 14:55:33 +0200

    Show shield when accessing <non reseller>.hostshield.net

--- html/index.php
+++ html/index.php
@@ -5,6 +5,23 @@ require __DIR__.'/../init.php';
 $host = explode('.', $_SERVER['HTTP_HOST']);
 define('account_username', $host[0]);
 
+// Show the plain index page if this is not a reseller login page
+$SQL = Connection::open('sql');
+if (!$SQL->exists('reseller WHERE username='.SQL(account_username))) {
+  $response = HTTP::get('https://username.snapbill.com/get.php?service=snapbill&username='.URL(account_username));
+  if ($response) {
+    $response = json_decode($response, True);
+    if ($response['state'] == 'active') {
+      $SQL->insert('reseller', array('username'=>account_username));
+    }else $response = NULL;
+  }
+
+  if (!$response) {
+    file_get_contents(__DIR__.'/plain/index.htm');
+    exit(0);
+  }
+}
+
 // Check the current session
 Session::start();
 
